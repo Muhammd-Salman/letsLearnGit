4c8edaf95efc850ca863ac3132ccdbce3510a067
Muhammad Salman Ahmad <isalman.ahmad01@gmail.com>
2022-04-23 12:03:32 +0500

    taking username in salman.cpp

---
+++ salman.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout <<"hello world"<<endl;
+    string name;
+    cout <<"enter name"<<endl;
+    cin >> name;
+    cout <<"your name is : "<<name<<endl;
+
+}
